eef8e1584ba6bb787510efcf4a25aa046a457d7e
RodricHil <rodrick.hilarian@proaccuracy.com>
2025-10-17 15:57:25 +0530

    step 3 - handle two commaseparated numbers

--- src/test/stringCalculator.test.ts
+++ src/test/stringCalculator.test.ts
@@ -7,7 +7,12 @@ describe("StringCalculator", () => {
   // });
 
   // step-2
-  test("should return the number itself if only one number", () => {
-    expect(add("1")).toBe(1);
+  // test("should return the number itself if only one number", () => {
+  //   expect(add("1")).toBe(1);
+  // });
+
+  // step-3
+  test("should return sum of two comma-separated numbers", () => {
+    expect(add("1,2")).toBe(3);
   });
 });
--- src/utils/stringCalculator.ts
+++ src/utils/stringCalculator.ts
@@ -1,4 +1,11 @@
 export function add(numbers: string): number {
+  // step 1 & 2
+  // if (!numbers) return 0;
+  // return Number(numbers);
+
+  // step 3
   if (!numbers) return 0;
-  return Number(numbers);
+  const parts = numbers.split(",");
+  const nums = parts.map((n) => Number(n));
+  return nums.reduce((a, b) => a + b, 0);
 }
